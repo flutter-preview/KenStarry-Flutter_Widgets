3446db45bff90654e906302bcff2919229106812
KenStarry <starrycodes@gmail.com>
2023-06-18 23:49:32 +0300

    Animated builder

---
+++ lib/widgets/animmated_builder.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AnimatedBuilder extends StatefulWidget {
+  const AnimatedBuilder({super.key});
+
+  @override
+  State<AnimatedBuilder> createState() => _AnimatedBuilderState();
+}
+
+class _AnimatedBuilderState extends State<AnimatedBuilder> {
+
+  bool selected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return ;
+  }
+}
