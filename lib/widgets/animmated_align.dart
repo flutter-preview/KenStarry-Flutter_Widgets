5b6d96d8a5539701b96a707a366f25c8c828721d
KenStarry <starrycodes@gmail.com>
2023-06-18 23:47:16 +0300

    Animated icon

---
+++ lib/widgets/animmated_align.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class AnimatedAlign extends StatefulWidget {
+  const AnimatedAlign({super.key});
+
+  @override
+  State<AnimatedAlign> createState() => _AnimatedAlignState();
+}
+
+class _AnimatedAlignState extends State<AnimatedAlign> {
+
+  bool selected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (){
+        setState(() {
+          selected = !selected;
+        });
+      },
+      child: Center(
+        child: Container(
+          width: double.infinity,
+          height: 200,
+        ),
+      ),
+    );
+  }
+}
